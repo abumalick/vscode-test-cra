1a787e8cf3513fa4e0b6d891a405a5330af5baac
abumalick <abdullah.hilson@gmail.com>
2019-10-28 08:16:42 +0000

    test: show that vscode-jest don't load setupTests.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import 'mutationobserver-shim'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test("Mutation Observer should be defined", () => {
+  expect(window.MutationObserver).toBeDefined()
+})
